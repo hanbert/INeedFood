eb26859215910d025b31c52456c1e8e4e427caf2
玉韬 <hanxianghui.hxh@antgroup.com>
2022-04-08 06:51:56 +0800

    ad meituan script

--- meituan.js
+++ meituan.js
@@ -22,23 +22,23 @@ const start = () => {
 		if (hasText('结算')) {
 			clickSettle('结算')
 			sleep(1000)
-		} else if (hasText('我知道了') || hasText('返回购物车') || hasText('站点自提')) {
+		} else if (hasText('当前不在可下单时段')) {
+			clickSettle('我知道了')
+			sleep(500)
+		} else if (hasText('前方拥堵') || hasText('我知道了') || hasText('返回购物车') || hasText('站点自提')) {
 			back()
 			sleep(500)
 		} else if (hasText('重新加载')) {
 			clickSettle('重新加载')
-			sleep(50)
-		} else if (hasText('提交订单')) {
-			var flag = className("android.widget.TextView").textStartsWith('提交订单').findOne().parent().click()
-			var sendTime = textStartsWith("1").findOnce();
-			if (sendTime) {
-				sendTime.parent().click()
-			} else {
-				back()
-			}
-			while(!flag) {
-				flag = className("android.widget.TextView").textStartsWith('提交订单').findOne().parent().click()
-			}
+			sleep(500)
+		} else if(hasText('选择收货地址')) {
+			back()
+			sleep(500)
+		} else if (hasText('立即支付')) {
+			clickSettle('立即支付')
+			sleep(500)
+		} else if (hasText('确认支付')) {
+			isSuccess = true
 			sleep(500)
 		}
 	}
